67114c51249a6e793f2c83214ee65c1bdaf9317a
adamrk <ark.email@gmail.com>
2017-03-17 13:15:49 -0400

    support Ellipses in macros

--- src/Macro.hs
+++ src/Macro.hs
@@ -2,70 +2,121 @@ module Macro where
 
 import Parser2
 import qualified Data.Map as M
+import Data.Maybe (isJust)
+import Control.Applicative (liftA2)
 
-call2Macro :: GenExpr a -> GenExpr a
-call2Macro (Call (Var x _) xs) = MacroUse (Var x ()) (map convMacro xs)
-
-convMacro :: GenExpr a -> GenDatum a
+convMacro :: GenExpr () -> GenDatum ()
 convMacro (Var s a) = SimpleDatum (Var s a)
 convMacro (Literal x) = SimpleDatum (Literal x)
-convMacro (Call (Var s a) xs) = CompoundDatum $ SimpleDatum (Var s a)
-                                  : map convMacro xs
+convMacro (Call x xs) = CompoundDatum $ convMacro x : map convMacro xs
+convMacro (Lambda vs b) = CompoundDatum $ SimpleDatum (Var "lambda" ())
+  : CompoundDatum (map convMacro vs) : [convMacroBody b]
+convMacro (Cond a b c) = CompoundDatum $ SimpleDatum (Var "if" ())
+  : convMacro a : convMacro b : [convMacro c]
+convMacro (Assign a b) = CompoundDatum $ SimpleDatum (Var "set!" ())
+  : convMacro a : [convMacro b]
 
-convAllMacros :: GenExpr a -> GenExpr a
-convAllMacros (Var x y) = Var x y
-convAllMacros (Literal x) = Literal x
-convAllMacros c@(Call (Var x y) xs)
-  | x `elem` macros = call2Macro c
-  | otherwise = Call (Var x y) (map convAllMacros xs)
-convAllMacros (Call f xs) = Call (convAllMacros f) (map convAllMacros xs)
-convAllMacros (Lambda xs b) = Lambda xs (convMacroBody b)
-convAllMacros (Cond a b c) = Cond (convAllMacros a) (convAllMacros b)
-                                      (convAllMacros c)
+convMacroBody :: GenBody () -> GenDatum ()
+convMacroBody (Body ds es) = CompoundDatum $ map convMacroDef ds 
+  ++ map convMacro es
 
-convMacroBody :: GenBody a -> GenBody a
-convMacroBody (Body ds es) = Body (map convMacroDef ds) (map convAllMacros es)
+convMacroDef :: GenDef () -> GenDatum ()
+convMacroDef (Def1 x y) = CompoundDatum $ [ SimpleDatum (Var "define" ())
+                                          , SimpleDatum x
+                                          , convMacro y
+                                          ]
+convMacroDef (Def2 x ys b) = CompoundDatum $ [ SimpleDatum (Var "define" ())
+                                             , CompoundDatum $ SimpleDatum x :
+                                                map SimpleDatum ys
+                                             , convMacroBody b
+                                             ]
+convMacroDef (Def3 ds) = CompoundDatum $ SimpleDatum (Var "begin" ()) : 
+  map convMacroDef ds                                      
 
-convMacroDef :: GenDef a -> GenDef a
-convMacroDef (Def1 x y) = Def1 x (convAllMacros y)
-convMacroDef (Def2 x ys b) = Def2 x ys (convMacroBody b)
-convMacroDef (Def3 ds) = Def3 $ map convMacroDef ds
-
-macros = ["or"]
-
-data Pattern = PatternId String | PatternDat Lit | PatternComp [Pattern]
+data Pattern = PatternId String 
+             | PatternDat Lit 
+             | PatternComp [Pattern]
+             | PatternLit String
+             | Ellipses
   deriving (Eq, Show)
 
-match :: Pattern -> GenDatum a -> Maybe (M.Map String (GenDatum a))
-match (PatternComp ps) (CompoundDatum ds) = 
-  if length ps == length ds
-    then fmap M.unions . sequence $ zipWith match ps ds
-    else Nothing
+data Matched a = Matched (M.Map String (GenDatum a)) [Matched a]
+  deriving (Eq, Show)
+
+extZipWith :: (a -> a -> a) -> [a] -> [a] -> [a]
+extZipWith f (b:bs) (c:cs) = f b c : extZipWith f bs cs
+extZipWith f [] cs = cs
+extZipWith f bs [] = bs
+
+-- | Combine two match results
+--
+combine :: Matched a -> Matched a -> Matched a
+combine (Matched m1 l1) (Matched m2 l2) = 
+  Matched (M.union m1 m2) (extZipWith combine l1 l2)
+
+-- | Fills in a pattern from a GenDatum that matches it
+--
+match :: Pattern -> GenDatum a -> Maybe (Matched a)
+match (PatternComp [x, Ellipses]) (CompoundDatum ds) = 
+  Matched M.empty <$> traverse (match x) ds
+match (PatternComp (x:xs)) (CompoundDatum (d:ds)) = 
+  combine <$> (match x d) <*> (match (PatternComp xs) (CompoundDatum ds))
+match (PatternComp []) (CompoundDatum []) = Just $ Matched M.empty []
+match (PatternComp []) _ = Nothing
 match (PatternDat x) (SimpleDatum (Literal y)) = 
   if x == y
-    then Just M.empty
+    then Just $ Matched M.empty []
     else Nothing
-match (PatternId s) x = Just $ M.singleton s x
+match (PatternId s) x = Just $ Matched (M.singleton s x) []
+match (PatternLit s) (SimpleDatum (Var x _)) = if x == s 
+                                              then Just $ Matched M.empty [] 
+                                              else Nothing 
 match _ _ = Nothing
 
-useTemplate :: Pattern -> M.Map String (GenDatum ()) -> GenDatum ()
-useTemplate (PatternComp xs) m = CompoundDatum $ map (flip useTemplate m) xs
-useTemplate (PatternDat x) _ = SimpleDatum (Literal x)
-useTemplate (PatternId s) m = case M.lookup s m of
-  Nothing -> SimpleDatum (Var s ())
-  Just d -> d
+getDat :: GenDatum () -> [GenDatum ()]
+getDat (CompoundDatum xs) = xs
+getDat _ = []
+
+useTemplate :: Pattern -> Matched () -> Maybe (GenDatum ())
+useTemplate (PatternComp [a, Ellipses]) (Matched m ls) =
+  CompoundDatum <$> (sequence . filter isJust) (map (useTemplate a) ls)
+useTemplate (PatternComp (x:xs)) m = 
+  let a = useTemplate x m
+      b = useTemplate (PatternComp xs) m
+  in  CompoundDatum <$> liftA2 (:) a (getDat <$> b)
+useTemplate (PatternComp []) m = Just $ CompoundDatum []
+useTemplate (PatternDat x) _ = Just $ SimpleDatum (Literal x)
+useTemplate (PatternId s) (Matched m l) = M.lookup s m
+useTemplate (PatternLit s) _ = Just $ SimpleDatum (Var s ())
+
+applyMacro :: String -> Pattern -> Pattern -> GenDatum () -> Maybe (GenDatum ())
+applyMacro key pat temp dat = match pat dat >>= useTemplate temp
+
+type MacroList = [(String, Pattern, Pattern)]
+
+applyMacros :: MacroList -> GenDatum () -> GenDatum ()
+applyMacros ms ex = 
+  let conversions = map (\(k, p, t) -> applyMacro k p t ex) ms
+  in  case take 1 $ filter isJust conversions of
+        [Just x] ->  applyMacros ms x
+        [] -> case ex of
+          CompoundDatum ds -> CompoundDatum $ map (applyMacros ms) ds
+          SimpleDatum y -> SimpleDatum y
 
 convDatum :: GenDatum () -> GenExpr ()
 convDatum (SimpleDatum x) = x
 convDatum (CompoundDatum (SimpleDatum (Var "if" _) : [a,b,c])) = 
   Cond (convDatum a) (convDatum b) (convDatum c)
-convDatum (CompoundDatum (SimpleDatum (Var "lambda" _) : xs : ys)) = undefined
+convDatum (CompoundDatum (SimpleDatum (Var "lambda" _) 
+  : (CompoundDatum xs) : [y])) = 
+    Lambda (map convDatum xs) (convDatumBody y)
 convDatum (CompoundDatum ( SimpleDatum (Var "set!" _) 
                          : SimpleDatum (Var x _) 
                          : [y])) = 
   Assign (Var x ()) (convDatum y)
-convDatum (CompoundDatum (SimpleDatum (Var x _) : xs)) =
-  Call (Var x ()) (map convDatum xs)
+convDatum (CompoundDatum (x: xs)) =
+  Call (convDatum x) (map convDatum xs)
+
 
 isDef :: GenDatum a -> Bool
 isDef (CompoundDatum (SimpleDatum (Var "define" _) : _)) = True
@@ -90,5 +141,14 @@ convDatumBody (CompoundDatum (x:xs)) =
          in   Body (convDatumDef x : ds) es
     else Body [] (map convDatum (x:xs))  
 
-replaceMacros :: GenExpr a -> GenExpr a
-replaceMacros = undefined
+applyMacrosExpr :: MacroList -> GenExpr () -> GenExpr ()
+applyMacrosExpr ms = convDatum . applyMacros ms . convMacro
+
+applyMacrosDef :: MacroList -> GenDef () -> GenDef ()
+applyMacrosDef ms = convDatumDef . applyMacros ms . convMacroDef
+
+applyMacrosProgram :: MacroList -> [CommOrDef] -> [CommOrDef]
+applyMacrosProgram ms xs = map (\x ->
+  case x of
+    Comm y -> Comm $ applyMacrosExpr ms y
+    Def y -> Def $ applyMacrosDef ms y) xs
